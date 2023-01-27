14bd52474c3653262182309d8c6e39410de0de21
Глеб Коваленко <glebkovalenko@MacBook-Air-Gleb.local>
2023-01-27 17:16:35 +0300

    init

---
+++ PhraseGenerator/PhraseGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhraseGeneratorApp.swift
+//  PhraseGenerator
+//
+//  Created by Глеб Коваленко on 27.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhraseGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
